cb7b16b009e0a1a9e1932faaf4c3b97c65be8457
Pablofl0 <A24pabloFL@iessanclemente.net>
2024-10-02 10:19:24 +0200

    2/10

---
+++ ud01/Matematica.java
@@ -0,0 +1,18 @@
+//Crear  programa que:
+//calcule raíz cuadrada de 234324
+//calcule 34 elevado a 17
+
+
+
+/**
+ * Matematica
+ */
+public class Matematica {
+    
+    public static void main(String[] args) {
+        double raiz;
+        raiz = Math.sqrt(234324);
+        System.out.println(raiz);
+        System.out.println(Math.pow(34, 17));
+    }
+}
