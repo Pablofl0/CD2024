2584a3f4f479fb1f790ffe4f5ba9677e3b077554
Pablofl0 <A24pabloFL@iessanclemente.net>
2024-10-24 12:41:42 +0200

    23/10

--- ud01/ciclovida/Suma.java
+++ ud01/ciclovida/Suma.java
@@ -1,12 +1,31 @@
 public class Suma {
     public static void main(String[] args) {
-            // Convertir los argumentos a números
-            double numero1 = Double.parseDouble(args[0]);
-            double numero2 = Double.parseDouble(args[1]);
+            //Definición de variables.
+            double numero1, numero2, numero3, suma;
 
-            // Calcular la suma
-            double suma = numero1 + numero2;
+            //Inicialización de variables.
+            numero1=0;
+            numero2=0;
+            numero3=0;
+            suma=0;
 
+            try {
+                // Convertir los argumentos a números
+            numero1 = Double.parseDouble(args[0]);
+            numero2 = Double.parseDouble(args[1]);
+
+            suma = numero1 + numero2;
+
+            if(args.length==3){
+                numero3 = Double.parseDouble(args[2]);
+                suma = suma + numero3;
+            }
+
+            } catch (Exception e) {
+                System.out.println("Error");
+            }
+
+            
             // Mostrar el resultado
             System.out.println(suma);
     }
---
+++ ud02/IDE.md
@@ -0,0 +1,25 @@
+# Instalación de un IDE en Linux
+
+- ## Razones para seleccionar un IDE concreto
+
+    Existen distintos motivos:
+    - Herramientas de desarrollo integradas.
+    - Editor de código inteligente.
+    - Asistente de inteligencia artificial.
+    - Rápida navegación y búsqueda.
+
+- ## Proceso de instalación
+
+    Para la versión gratuita:
+    1. Abrir terminal.
+    1. Introducir el siguiente comando: sudo snap install pycharm-community --classic
+
+    En caso de la versión de pago:
+    1. Abrir terminal.
+    1. Introducir el siguiente comando: sudo snap install pycharm-professional --classic
+
+- ## Programa sencillo 
+
+    Programa que imprime Hola mundo en el terminal:
+
+    ![Captura programa en funcionamiento](screen.jpg)
