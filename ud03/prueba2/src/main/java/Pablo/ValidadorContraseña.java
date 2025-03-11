6a85088e18d08f9ffd3c00808df318768a94399f
Pablofl0 <A24pabloFL@iessanclemente.net>
2025-03-11 09:00:23 +0100

    tdd

--- ud03/prueba2/src/main/java/Pablo/ValidadorContraseña.java
+++ ud03/prueba2/src/main/java/Pablo/ValidadorContraseña.java
@@ -15,23 +15,23 @@ public class ValidadorContraseña {
         boolean numero = false;
         boolean especial = false;
 
-        if (contraseña.length() == 8) {
+        if (contraseña.length() >= 8) {
             for (int i = 0; i < contraseña.length(); i++) {
-                if ((int)contraseña.charAt(i) >= (int)'A' && (int)contraseña.charAt(i) <= (int)'Z') {
-                    mayus = true;
-                }
-                if ((int)contraseña.charAt(i) >= (int)'a' && (int)contraseña.charAt(i) <= (int)'z') {
-                    minus = true;
-                }
                 if ((int)contraseña.charAt(i) >= (int)'0' && (int)contraseña.charAt(i) <= (int)'9') {
                     numero = true;
                 }
-                if (!(((int)contraseña.charAt(i) >= (int)'A' && (int)contraseña.charAt(i) <= (int)'Z') && ((int)contraseña.charAt(i) >= (int)'a' && (int)contraseña.charAt(i) <= (int)'z') && ((int)contraseña.charAt(i) >= (int)'0' && (int)contraseña.charAt(i) <= (int)'9'))) {
+                else if ((int)contraseña.charAt(i) >= (int)'a' && (int)contraseña.charAt(i) <= (int)'z') {
+                    minus = true;
+                }
+                else if ((int)contraseña.charAt(i) >= (int)'A' && (int)contraseña.charAt(i) <= (int)'Z') {
+                    mayus = true;
+                }
+                else /*(!(((int)contraseña.charAt(i) >= (int)'A' && (int)contraseña.charAt(i) <= (int)'Z') && ((int)contraseña.charAt(i) >= (int)'a' && (int)contraseña.charAt(i) <= (int)'z') && ((int)contraseña.charAt(i) >= (int)'0' && (int)contraseña.charAt(i) <= (int)'9'))) */{
                     especial = true;
                 }
             }
         }
-        if (especial == true && mayus == true && minus == true && numero == true) {
+        if (mayus && minus && numero && especial) {
             return true;
         }
         return false;
